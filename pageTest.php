c1adf3a19e5e0fa2fd6c7d3bed8d1602a221eb7f
bferry <ferrybryan@hotmail.fr>
2017-01-24 09:58:52 +0100

    modification de pageTest.php pour afficher un nombre aleatoire

--- pageTest.php
+++ pageTest.php
@@ -2,6 +2,8 @@
 <html>
 <head>
 	<title>Ceci est une page de test et il est <?php echo date('h:i:s'); ?></title>
+	<meta charset="utf-8" />
+	<meta name="description" content="un chiffre aleatoire entre 1 et 10 grâce à php :<?php rand(1,10); ?>"/>
 </head>
 <body>
 
