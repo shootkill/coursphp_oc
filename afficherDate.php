a7df13d7622f64b8afc69e81129beb731a4cf144
bferry <ferrybryan@hotmail.fr>
2017-01-24 09:04:01 +0100

    afficherDate.php

---
+++ afficherDate.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>creationDate</title>
+</head>
+<body>
+	<h1>Ma page web</h1>
+	<p>Aujourd'hui, nous somme <?php echo date('d/m/y h:i:s'); ?></p>
+</body>
+</html>
